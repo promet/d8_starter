ce345c1656ab9bc9c2da3e953d29f70958c9495b
acouch <acinternets@gmail.com>
2020-08-21 10:03:09 -0400

    Fixes ddev

--- src/sites/default/settings.promet.php
+++ src/sites/default/settings.promet.php
@@ -2,72 +2,98 @@
 
 // @codingStandardsIgnoreFile
 
+/******************** DRUPAL SETTINGS ********************/
+$settings['install_profile'] = 'minimal';
+$settings['hash_salt'] = 'ts3MpgAEZcYSm0_tCkJgrYiEMg1rRSHPzyrXRHLGTg7uBOHXRojaOnIfu1sQJO4hWxZVGJSqTA';
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
+$settings['entity_update_batch_size'] = 50;
+$settings['locale_custom_strings_en']['Address label'] = [
+  'Company' => 'Name',
+];
 $config_directories[CONFIG_SYNC_DIRECTORY] = '../config/default';
+$settings['config_sync_directory'] = '../config/default';
+$settings['config_vcs_directory'] = '../config/default';
+$settings['update_free_access'] = FALSE;
+
+/******************** ENVIRONMENT SETTINGS ********************/
 $site_env = getenv('SITE_ENVIRONMENT') ? getenv('SITE_ENVIRONMENT') : 'local';
 $acquia_env = getenv('AH_SITE_ENVIRONMENT') ? getenv('AH_SITE_ENVIRONMENT') : '';
+$site = $site_path;
 
-## Disable split config settings.  Will turn on correct one later.
-$config['config_split.config_split.local']['status'] = FALSE;
-$config['config_split.config_split.dev']['status'] = FALSE;
-$config['config_split.config_split.stage']['status'] = FALSE;
-$config['config_split.config_split.prod']['status'] = FALSE;
+// Colors.
+$black = '000000';
+$white = '#FFFFFF';
+$lightGreen = '#42FF33';
+$blue = '#0000FF';
+$violet = '#FF00FF';
+$yellow = '#FFFF00';
+$orange = '#FFAA00';
+$red = '#FF0400';
 
 ## Sets the colors for the Environment Indicator module based on environment.
 // Local Developer Environments (green background, black text)
 if ($site_env == 'local') {
-  $config['environment_indicator.indicator']['bg_color'] = '#42FF33';
-  $config['environment_indicator.indicator']['fg_color'] = '#000000';
+  $config['environment_indicator.indicator']['bg_color'] = $lightGreen;
+  $config['environment_indicator.indicator']['fg_color'] = $black;
   $config['environment_indicator.indicator']['name'] = 'Local';
-  $config['config_split.config_split.local']['status'] = TRUE;
+  $config['search_api.index.site_search']['server'] = 'solr';
+  $config['search_api.server.acquia_search_server']['status'] = false;
+  $config['search_api.server.solr']['status'] = true;
 }
 // prometdev environment (blue background, white text)
 elseif ($site_env == 'prometdev') {
-  $config['environment_indicator.indicator']['bg_color'] = '#0000FF';
-  $config['environment_indicator.indicator']['fg_color'] = '#FFFFFF';
+  $config['environment_indicator.indicator']['bg_color'] = $blue;
+  $config['environment_indicator.indicator']['fg_color'] = $white;
   $config['environment_indicator.indicator']['name'] = 'Promet Dev';
-  $config['config_split.config_split.dev']['status'] = TRUE;
 }
 // prometstaging environment (pink background, black text)
 elseif ($site_env == 'prometstg') {
-  $config['environment_indicator.indicator']['bg_color'] = '#FF00FF';
-  $config['environment_indicator.indicator']['fg_color'] = '#000000';
+  $config['environment_indicator.indicator']['bg_color'] = $violet;
+  $config['environment_indicator.indicator']['fg_color'] = $black;
   $config['environment_indicator.indicator']['name'] = 'Promet Staging';
-  $config['config_split.config_split.stage']['status'] = TRUE;
-}
-// All Acquia envs.
-if ($acquia_env) {
-  if (file_exists('/var/www/site-php')) {
-    require '/var/www/site-php/orangecounty/orangecounty-settings.inc';
-  }
 }
 // Acquia dev environment (yellow background, black text)
 if ($acquia_env == 'dev') {
-  $config['environment_indicator.indicator']['bg_color'] = '#FFFF00';
-  $config['environment_indicator.indicator']['fg_color'] = '#000000';
+  $config['environment_indicator.indicator']['bg_color'] = $yellow;
+  $config['environment_indicator.indicator']['fg_color'] = $black;
   $config['environment_indicator.indicator']['name'] = 'Acquia Dev';
-  $config['config_split.config_split.dev']['status'] = TRUE;
+  $config['search_api.index.site_search']['server'] = 'acquia_search_server';
+  $config['search_api.server.acquia_search_server']['status'] = true;
+  $config['search_api.server.solr']['status'] = false;
 }
 // Acquia stage environment (orange background, black text)
 elseif ($acquia_env == 'test') {
-  $config['environment_indicator.indicator']['bg_color'] = '#FFAA00';
-  $config['environment_indicator.indicator']['fg_color'] = '#000000';
+  $config['environment_indicator.indicator']['bg_color'] = $orange;
+  $config['environment_indicator.indicator']['fg_color'] = $black;
   $config['environment_indicator.indicator']['name'] = 'Acquia QA';
-  $config['config_split.config_split.stage']['status'] = TRUE;
+  $config['search_api.index.site_search']['server'] = 'acquia_search_server';
+  $config['search_api.server.acquia_search_server']['status'] = true;
+  $config['search_api.server.solr']['status'] = false;
 }
 // Acquia prod environment (red background, black text)
 elseif ($acquia_env == 'prod') {
-  $config['environment_indicator.indicator']['bg_color'] = '#FF0400';
-  $config['environment_indicator.indicator']['fg_color'] = '#000000';
+  $config['environment_indicator.indicator']['bg_color'] = $red;
+  $config['environment_indicator.indicator']['fg_color'] = $black;
   $config['environment_indicator.indicator']['name'] = 'Acquia  Production';
-  $config['config_split.config_split.prod']['status'] = TRUE;
+  $config['search_api.index.site_search']['server'] = 'acquia_search_server';
+  $config['search_api.server.acquia_search_server']['status'] = true;
+  $config['search_api.server.solr']['status'] = false;
 }
 
 ## End Environment Indicator settings.
-if (getenv('MEMCACHE_ENABLED'))  {
+
+// Memcache settings.
+if ($acquia_env) {
+  if (file_exists(DRUPAL_ROOT . '/sites/cloud-memcache-d8.php')) {
+    require(DRUPAL_ROOT . '/sites/cloud-memcache-d8.php');
+  }
+}
+elseif (getenv('MEMCACHE_ENABLED'))  {
   $settings['memcache']['servers'] = ['memcached:11211' => 'default'];
   $settings['memcache']['bins'] = ['default' => 'default'];
-  $settings['memcache']['key_prefix'] = '';
   $settings['cache']['default'] = 'cache.backend.memcache';
 }
-
-$settings['update_free_access'] = FALSE;
